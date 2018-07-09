d2f9311a95b6f0756917e76f74c4c7956ec5ea64
Logan <shadow_1226@163.com>
2018-07-09 23:50:23 +0800

    睡觉

--- src/api/customer.js
+++ src/api/customer.js
@@ -3,6 +3,14 @@ import request from '@/utils/request'
 export function getCustomerList() {
   return request({
     url: '/sys/customer/list',
-    type: 'post'
+    method: 'post'
+  })
+}
+
+export function addCustomer(form) {
+  return request({
+    url: '/sys/customer/add',
+    method: 'post',
+    data: form
   })
 }
--- src/views/customer/index.vue
+++ src/views/customer/index.vue
@@ -16,18 +16,20 @@
         <el-button type="success" size="small" @click="editProduct('customerForm')">修改</el-button>
         <el-button type="success" size="small" @click="deleteProduct('customerForm')">删除</el-button>
       </div>
-      <el-table :data="currentProductList" border style="width: 100%">
+      <el-table :data="currentProductList" border>
 
         <el-table-column type="index">
         </el-table-column>
         <el-table-column prop="model" label="产品型号">
         </el-table-column>
-        <el-table-column prop="package" label="包装">
+        <el-table-column prop="packaging" label="包装">
+        </el-table-column>
+        <el-table-column prop="unitPrice" label="单价">
         </el-table-column>
       </el-table>
     </div>
     <el-dialog title="客户添加" :visible.sync="dialogVisible" width="360px">
-      <el-form :model="form" ref="customerForm">
+      <el-form :model="form" ref="customerForm" label-width="80px" label-position="left">
         <el-form-item label="客户名称" prop="customerName">
           <el-input v-model="form.customerName" auto-complete="off"></el-input>
         </el-form-item>
@@ -43,22 +45,42 @@
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="cusSubmit">确 定</el-button>
+      </div>
+    </el-dialog>
+    <el-dialog title="产品添加" :visible.sync="dialogVisible2" width="360px">
+      <el-form :model="form" ref="customerForm" label-width="80px" label-position="left">
+        <el-form-item label="客户名称" prop="customerName">
+          <el-input v-model="form.customerName" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="地址" prop="address">
+          <el-input v-model="form.address" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" prop="tel">
+          <el-input v-model="form.tel" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="form.mobile" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="cusSubmit">确 定</el-button>
       </div>
     </el-dialog>
   </div>
 </template>
 <script>
-import { getCustomerList } from '@/api/customer'
+import { getCustomerList, addCustomer } from '@/api/customer'
+import customerList from '@/components/tables/customerList'
 export default {
   name: 'customer',
+  components: {
+    customerList
+  },
   data() {
     return {
-      customerList: [
-        { customerName: '张三' },
-        { customerName: '李四' },
-        { customerName: '王五' }
-      ],
+      customerList: [],
       currentProductList: [],
       productList: [
         { customerName: '张三', model: 'X1', package: '白盒', unitPrice: '20' },
@@ -68,7 +90,8 @@ export default {
         customerName: '',
         address: '',
         tel: '',
-        mobile: ''
+        mobile: '',
+        type: 1
       },
       dialogVisible: false
     }
@@ -83,11 +106,16 @@ export default {
         return item.customerName === row.customerName
       })
     },
+    cusSubmit() {
+      addCustomer(this.form).then(response => {
+        this.$message('提交成功')
+        this.dialogVisible = false
+        this.loadingCustomerList()
+      })
+    },
     loadingCustomerList() {
       getCustomerList().then(response => {
-        if (response.code === 200) {
-          this.customerList = response.data.list
-        }
+        this.customerList = response.data.list
       })
     }
   },
@@ -100,6 +128,7 @@ export default {
 .table-layout {
   display: flex;
   align-items: flex-start;
+  height:100%;
   .left-table {
     flex: 0 0 auto;
     height: 50px;
